f100c5f2c06e24abfc36ebd26789fbf7c047db4a
NingWang <594422814@qq.com>
2019-02-27 21:24:46 +0800

    Update init_UDT.m

--- train/training/init_UDT.m
+++ train/training/init_UDT.m
@@ -20,6 +20,8 @@ net.meta.inputSize = opts.inputSize;
 net.meta.padding = opts.padding;
 
 %% network structure
+% network training needs three patches, therefore three shared-weight networks
+
 %% target 
 convx1_1 = dagnn.Conv('size', [3 3 3 32], 'pad', 0, 'stride', 1, 'dilate', 1, 'hasBias', true) ;
 net.addLayer('convx1_1', convx1_1, {'target'}, {'convx1_1'}, {'conv1f', 'conv1b'}) ;
@@ -28,6 +30,7 @@ net.addLayer('relux1_1', dagnn.ReLU(), {'convx1_1'}, {'convx1_1_relu'});
 convx1_2 = dagnn.Conv('size', [3 3 32 32], 'pad', 0, 'stride', 1, 'dilate', 1, 'hasBias', true) ;
 net.addLayer('convx1_2', convx1_2, {'convx1_1_relu'}, {'convx1_2'}, {'conv2f', 'conv2b'}) ;
 net.addLayer('norm_x1', dagnn.LRN('param',[5 1 0.0001/5 0.75]), {'convx1_2'}, {'x'});
+
 %% search 1
 convx2_1 = dagnn.Conv('size', [3 3 3 32], 'pad', 0, 'stride', 1, 'dilate', 1, 'hasBias', true) ;
 net.addLayer('convx2_1', convx2_1, {'search1'}, {'convx2_1'}, {'conv1f', 'conv1b'}) ;
@@ -36,6 +39,7 @@ net.addLayer('relux2_1', dagnn.ReLU(), {'convx2_1'}, {'convx2_1_relu'});
 convx2_2 = dagnn.Conv('size', [3 3 32 32], 'pad', 0, 'stride', 1, 'dilate', 1, 'hasBias', true) ;
 net.addLayer('convx2_2', convx2_2, {'convx2_1_relu'}, {'convx2_2'}, {'conv2f', 'conv2b'}) ;
 net.addLayer('norm_x2', dagnn.LRN('param',[5 1 0.0001/5 0.75]), {'convx2_2'}, {'z1'});
+
 %% search 2
 conv1s = dagnn.Conv('size', [3 3 3 32], 'pad', 0, 'stride', 1, 'dilate', 1, 'hasBias', true) ;
 net.addLayer('conv1s', conv1s, {'search2'}, {'conv1s'}, {'conv1f', 'conv1b'}) ;
@@ -45,6 +49,7 @@ conv2s = dagnn.Conv('size', [3 3 32 32], 'pad', 0, 'stride', 1, 'dilate', 1, 'ha
 net.addLayer('conv2s', conv2s, {'conv1sx'}, {'conv2s'}, {'conv2f', 'conv2b'}) ;
 net.addLayer('norm_s', dagnn.LRN('param',[5 1 0.0001/5 0.75]), {'conv2s'}, {'z2'});
 feature_sz = opts.inputSize([1,1]) - [4, 4];
+
 
 target_sz = opts.inputSize([1,1])/(1+opts.padding);
 sigma = sqrt(prod(target_sz))/10;
