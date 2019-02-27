3ce0a48d19c8c0f70244983f7e26d3e1746420c6
NingWang <594422814@qq.com>
2019-02-27 21:10:00 +0800

    Update training_env.m

--- train/training/training_env.m
+++ train/training/training_env.m
@@ -1,14 +1,9 @@
 function training_env()
 if isunix()
-    % addpath('users/wangning/Tracking/DCFNet-master/matconvnet/matlab') 
     addpath('/data3/wangning/matconvnet/matlab')    
 end
-
 % vl_compilenn('enableGPU', true);
-
 run('vl_setupnn.m') ;
-
 addpath(fullfile('..','utils'));
-
 fftw('planner','patient');
 end
